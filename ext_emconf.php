540a9eb76c359ffb2b933af4c232839c65dac449
Daniel Huf <huf.daniel@gmail.com>
2020-08-03 13:08:19 +0200

    [TASK] TYPO3 v10 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-0.0.0',
-            'typo3' => '7.6.*-9.5.99',
+            'typo3' => '8.7.13-10.9.99',
             'extbase' => '',
         ],
         'conflicts' => [
